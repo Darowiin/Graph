bcf62b70fcbaaa7965562f40828826b5c2346b59
Влад Цуненко <vlad.tsunenko.2004@mail.ru>
2024-05-10 00:37:14 +0400

    done test basic methods

---
+++ tests/GraphTests.cc
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include "Graph/Graph.h"
+
+class GraphTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        graph.add_vertex("A");
+        graph.add_vertex("B");
+        graph.add_vertex("C");
+        graph.add_vertex("D");
+
+        graph.add_edge("A", "B", 2.0);
+        graph.add_edge("A", "C", 1.5);
+        graph.add_edge("B", "D", 3.0);
+        graph.add_edge("C", "D", 2.5);
+    }
+
+    Graph<std::string, double> graph;
+};
+
+TEST_F(GraphTest, AddVertex) {
+    ASSERT_TRUE(graph.has_vertex("A"));
+    ASSERT_TRUE(graph.has_vertex("B"));
+    ASSERT_TRUE(graph.has_vertex("C"));
+    ASSERT_TRUE(graph.has_vertex("D"));
+}
+
+TEST_F(GraphTest, AddRemoveEdge) {
+    ASSERT_TRUE(graph.has_edge("A", "B"));
+    ASSERT_TRUE(graph.has_edge("A", "C"));
+    ASSERT_TRUE(graph.has_edge("B", "D"));
+    ASSERT_TRUE(graph.has_edge("C", "D"));
+
+    ASSERT_TRUE(graph.remove_edge("A", "B"));
+    ASSERT_FALSE(graph.has_edge("A", "B"));
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
